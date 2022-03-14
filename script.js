574d44d1b5683a9f5cf8e3eef9911347c5d4085e
jsilvabarb <jsilvabarb@gamil.com>
2022-03-14 18:22:26 -0300

    added pizza window area flex.

--- script.js
+++ script.js
@@ -10,10 +10,20 @@ pizzaJson.map((pizza, index) => {
   let pizzaItem = select('.models .pizza-item').cloneNode(true)
 
   pizzaItem.querySelector('.pizza-item--img img').src = pizza.img
-
   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2)}`
   pizzaItem.querySelector('.pizza-item--name').innerHTML = pizza.name
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizza.description
+
+  pizzaItem.querySelector('a').addEventListener('click', (e) => {
+    e.preventDefault()
+
+    select('.pizzaWindowArea').style.opacity = 0
+    select('.pizzaWindowArea').style.display = 'flex'
+    setTimeout(() => {
+      select('.pizzaWindowArea').style.opacity = 1
+    }, 200)
+
+  })
 
   select('.pizza-area').append( pizzaItem )
 
