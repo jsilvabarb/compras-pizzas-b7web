632b0118fcd3f2857841362fbef4e67264ddd67e
jsilvabarb <jsilvabarb@gamil.com>
2022-03-14 16:29:01 -0300

    added pizza info: image, price, name, description.

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ pizzaJson.map((pizza, index) => {
 
   let pizzaItem = select('.models .pizza-item').cloneNode(true)
 
+  pizzaItem.querySelector('.pizza-item--img img').src = pizza.img
+
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2)}`
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = pizza.name
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = pizza.description
+
   select('.pizza-area').append( pizzaItem )
 
 })
